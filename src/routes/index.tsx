8e990ac3b57e48975908c99b4066c7f63ec4d249
Ismail Courr <contact@ismailcourr.ml>
2023-04-01 03:52:35 +0000

    Add meta tags for SEO

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,5 @@
 import { component$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
   return <></>;
 });
-
-export const head: DocumentHead = {
-  title: 'Ismail Courr - Frontend Developer Portfolio',
-  meta: [
-    {
-      name: 'description',
-      content: "I’m a software developer. I love to build things that live on the internet, from websites to web apps. I specialize in building exceptional digital experiences, products, and services. I'm quietly confident, naturally curious, and perpetually working on improving my skills one at a time.",
-    },
-  ],
-};
