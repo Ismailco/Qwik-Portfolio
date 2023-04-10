aafb2c95b6a1b3533abf5315783e88abcf9f2202
Ismail Courr <contact@ismailcourr.ml>
2023-04-10 22:01:44 +0000

    ...

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -35,7 +35,7 @@ export const RouterHead = component$(() => {
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
       <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" /> */}
 
-      <link rel="preload" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&family=Roboto:wght@400;500;700&display=swap" as="style" onoad="this.onload=null;this.rel='stylesheet'" />
+      <link rel="preload" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&family=Roboto:wght@400;500;700&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'" />
       <noscript>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&family=Roboto:wght@400;500;700&display=swap" />
       </noscript>
