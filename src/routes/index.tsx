31b73989a318b7d119972f635c95f49af96dd6b0
Ismail Courr <contact@ismailcourr.ml>
2023-05-21 14:04:25 +0100

    Add skills section

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { component$, $ } from '@builder.io/qwik';
 import Header from '~/components/starter/header';
 import Projects from '~/components/starter/projects';
+import Skills from '~/components/starter/skills';
 
 export const sendEmail = $(() => {
   window.location = 'mailto:ismailcourr@gmail.com' as any;
@@ -15,6 +16,21 @@ export default component$(() => {
         <p class="-rotate-2 bg-[#f3f3f3] border shadow-md hover:shadow-xl duration-700 min-w-[1000%] h-16 flex justify-center items-center">Web Development ⚡ Frontend Development ⚡ Fullstack Development ⚡ Mobile Development ⚡ Web Development ⚡ Frontend Development ⚡ Fullstack Development⚡ Mobile Development ⚡</p>
       </div>
       <Projects />
+      <section class="w-full flex flex-col md:flex-row justify-start md:justify-around items-center py-10 my-10 bg-black text-white">
+        <div>
+          <p class="font-bold bg-clip-text bg-button-grad text-transparent">PROJECTS DONE</p>
+          <p class="text-4xl font-bold">10+</p>
+        </div>
+        <div>
+          <p class="font-bold bg-clip-text bg-button-grad text-transparent">EXPERIENCE</p>
+          <p class="text-4xl font-bold">1+ Years</p>
+        </div>
+        <div>
+          <p class="font-bold bg-clip-text bg-button-grad text-transparent">CLIENTS SATISFACTION</p>
+          <p class="text-4xl font-bold">100%</p>
+        </div>
+      </section>
+      <Skills />
       <section class="w-full flex flex-col md:flex-row justify-start md:justify-around items-center py-10 my-10 bg-black text-white">
         <h3 class="w-3/4 md:w-1/3 font-bold text-3xl mb-10 md:mb-0">
           Have An Awesome
