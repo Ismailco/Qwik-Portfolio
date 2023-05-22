6f8a2bbbe75e0576d62bea6b73e42c60f9ecb21a
Ismail Courr <contact@ismailcourr.ml>
2023-05-22 15:21:32 +0100

    Change the footer links

--- src/components/starter/contact/form.tsx
+++
@@ -1,8 +0,0 @@
-import { ElementEvent } from '@builder.io/qwik';
-
-export async function onSubmit(e: ElementEvent) {
-  e.preventDefault();
-  const form = e.target as HTMLFormElement;
-
-  console.log(new FormData(form));
-}
