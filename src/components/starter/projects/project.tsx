2aa4947ebd1b51a422038587a21b05beec7b064a
Ismail Courr <ismailcourr@gmail.com>
2024-06-21 20:40:41 +0100

    Fix backdrop issue

--- src/components/starter/projects/project.tsx
+++ src/components/starter/projects/project.tsx
@@ -12,7 +12,7 @@ export default component$(({ name, image, source, live, tech }: ProjectProps) =>
   return (
     <article class="w-96 md:w-full h-full rounded-md border border-gray-300 shadow-md hover:shadow-sm">
       <div class="opacity-0 hover:opacity-100 h-0 w-full relative">
-        <div class={`hover:backdrop-blur-sm rounded-md w-full h-full h-80 relative hover:bg-gray-300/70 flex justify-center items-center`}>
+        <div class={`hover:backdrop-blur-sm rounded-md w-full h-80 relative hover:bg-gray-300/70 flex justify-center items-center`}>
           <div class="text-center">
             <ul class="flex py-6 justify-center items-center">
               <li class="mr-5 bg-button-grad mx-1 rounded-full flex justify-center items-center p-0.5">
